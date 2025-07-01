047bef735824e5c977b9d687ef37002c57e26920
om verma <thevermaom@gmail.com>
2025-07-01 09:56:16 -0400

    Added Store and Carts Functions

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +65,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'category.context_processors.menu_links'
+                'category.context_processors.menu_links',
+                'carts.context_processors.counter',
+                
             ],
         },
     },
--- store/models.py
+++ store/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from category.models import category
 from django.urls import reverse
 
+
+class VariationManager(models.Manager):
+        def colors(self):
+            return super(VariationManager,self).filter(variation_category='color',is_active=True)
+     
+        def sizes(self):
+            return super(VariationManager,self).filter(variation_category='size',is_active=True)
+          
+
 class Product(models.Model):
     product_name    =models.CharField(max_length=200,unique=True)
     slug            =models.SlugField(max_length=200,unique=True)
@@ -22,7 +31,28 @@ class Product(models.Model):
     
     
     def __str__(self):
-        return self.product_name    
+        return self.product_name  
     
+    
+    
+
+
+variation_category_choice=(
+    ('color','color'),
+    ('size','size'),
+)       
+    
+class Variation(models.Model):
+    product             =models.ForeignKey(Product,on_delete=models.CASCADE)
+    variation_category  =models.CharField(max_length=100,choices=variation_category_choice)
+    variation_value     =models.CharField(max_length=100)
+    is_active           =models.BooleanField(default=True)
+    created_date        =models.DateTimeField(auto_now=True)
+    
+    
+    objects=VariationManager()
+       
+    def __str__(self):
+        return self.variation_value
     
     
